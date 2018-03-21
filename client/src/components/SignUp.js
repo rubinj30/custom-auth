c0dd362d44ed959f268a8730f4ef87eb3c9cf291
Jonathan Rubin <jonathan.a.rubin@gmail.com>
2018-03-20 22:29:50 -0400

    Added title, required inputs, links to buttons, and put all in form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
-import { LogoImg, CenterColumn, InputField, ButtonContainer, Button } from './styled-components/Styling'
+import { Link } from 'react-router-dom'
+import { LogoImg, CenterColumn, InputField, ButtonContainer, Button, ColumnTitle, StyledLink } from './styled-components/Styling'
 
 class SignUp extends Component {
     render() {
         return (
             <div>
                 <LogoImg width="200" src="https://assets.hmwallace.com//sources/images/supply_logo-unboxed.svg" alt="" />
-                <CenterColumn>
-                    <InputField placeholder="First Name"/>
-                    <InputField placeholder="Last Name"/>
-                    <InputField placeholder="E-mail"/>
-                    <InputField placeholder="Phone Number"/>
-                    <InputField placeholder="Password"/>
-                    <InputField placeholder="Confirm Password"/>
-                    <ButtonContainer>
-                        <Button>Sign Up</Button>
-                        <Button>Back to Home</Button>
-                    </ButtonContainer>
-                </CenterColumn>
+                <form action="">
+                    <CenterColumn>
+                        <ColumnTitle>Sign Up</ColumnTitle>
+                        <InputField placeholder="First Name" required />
+                        <InputField placeholder="Last Name" required />
+                        <InputField placeholder="E-mail" required />
+                        <InputField placeholder="Phone Number" required />
+                        <InputField placeholder="Password" required />
+                        <InputField placeholder="Confirm Password" required />
+                        <ButtonContainer>
+                            <Button>Sign Up</Button>
+                            <StyledLink to={'/'}><Button>Back to Home</Button></StyledLink>
+                        </ButtonContainer>
+                    </CenterColumn>
+                </form>
             </div>
         );
     }
