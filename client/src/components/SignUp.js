8d96467b9462730064fbd80af882bfff89c9773d
Jonathan Rubin <jonathan.a.rubin@gmail.com>
2018-03-21 11:53:56 -0400

    added state and handleChange for inputs

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import { LogoImg, CenterColumn, InputField, ButtonContainer, Button, ColumnTitle, StyledLink } from './styled-components/Styling'
 
 class SignUp extends Component {
+    state = {
+        newUser: {
+            firstName: '',
+            lastName: '',
+            emailAddress: '',
+            phoneNumber: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    handleChange = (event) => {
+        event.preventDefault()
+        const attributeName = event.target.name
+        const attributeValue = event.target.value
+        const newUser = { ...this.state.newUser }
+        newUser[attributeName] = attributeValue
+        this.setState({ newUser })
+    }
+
     render() {
         return (
             <div>
@@ -9,12 +29,18 @@ class SignUp extends Component {
                 <form action="">
                     <CenterColumn>
                         <ColumnTitle>Sign Up</ColumnTitle>
-                        <InputField placeholder="First Name" required />
-                        <InputField placeholder="Last Name" required />
-                        <InputField placeholder="E-mail" required />
-                        <InputField placeholder="Phone Number" required />
-                        <InputField placeholder="Password" required />
-                        <InputField placeholder="Confirm Password" required />
+                        <InputField onChange={this.handleChange} 
+                            placeholder="First Name" name="firstName" required />
+                        <InputField onChange={this.handleChange} 
+                            placeholder="Last Name" name="lastName" required />
+                        <InputField onChange={this.handleChange} 
+                            placeholder="E-mail" name="emailAddress" required />
+                        <InputField onChange={this.handleChange} 
+                            placeholder="Phone Number" name="phoneNumber" required />
+                        <InputField onChange={this.handleChange} 
+                            placeholder="Password" name="password" type="password" required />
+                        <InputField onChange={this.handleChange} 
+                            placeholder="Confirm Password" name="confirmPassword" type="password" required />
                         <ButtonContainer>
                             <Button>Sign Up</Button>
                             <StyledLink to={'/'}><Button>Back to Home</Button></StyledLink>
