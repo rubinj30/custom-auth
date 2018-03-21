7d8366d4951590fe59597ee1a026330e602253bb
Jonathan Rubin <jonathan.a.rubin@gmail.com>
2018-03-20 22:29:10 -0400

    Added title, required inputs, and links to buttons. And inputs in form.

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import { InputField, CenterColumn, LogoImg, Button, ButtonContainer } from './styled-components/Styling'
+import { Link } from 'react-router-dom'
+import { InputField, CenterColumn, LogoImg, Button, ButtonContainer, ColumnTitle, StyledLink } from './styled-components/Styling'
 
 class LogIn extends Component {
     render() {
         return (
             <div>
                 <LogoImg width="200" src="https://assets.hmwallace.com//sources/images/supply_logo-unboxed.svg" alt="" />
-
-                <CenterColumn>
-                    <InputField placeholder="username" />
-                    <InputField placeholder="password" />
-                    <ButtonContainer>
-                        <Button>Log In</Button>
-                        <Button>Back to Home</Button>
-                    </ButtonContainer>
-                </CenterColumn>
+                <form action="">
+                    <CenterColumn>
+                        <ColumnTitle>Log In</ColumnTitle>
+                        <InputField placeholder="username" required/>
+                        <InputField placeholder="password" required/>
+                        <ButtonContainer>
+                            <Button>Log In</Button>
+                            <StyledLink to={'/'}><Button>Back to Home</Button></StyledLink>
+                        </ButtonContainer>
+                    </CenterColumn>
+                </form>
             </div>
-                    );
-                }
-            }
-            
+        );
+    }
+}
+
 export default LogIn;
